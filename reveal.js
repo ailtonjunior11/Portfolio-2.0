36029f339fc034da09249f7cb7e77654e5cd5057
ailtonjunior11 <ailtonjunior.dev.frontend@gmail.com>
2024-06-12 13:34:02 -0300

    Novo arquivo js criado

---
+++ reveal.js
@@ -0,0 +1,112 @@
+window.revelar = ScrollReveal({ reset: true });
+
+// Topo da página
+revelar.reveal(".efeito-texto-topo", {
+  duration: 2000,
+  distance: "90px",
+});
+
+revelar.reveal(".efeito-img-topo", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+});
+
+// Titulos das sessões
+revelar.reveal(".titulo", {
+  duration: 2000,
+  distance: "90px",
+});
+
+// Sessão especialidades
+revelar.reveal(".efeito-especialidades1", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+});
+
+revelar.reveal(".efeito-especialidades2", {
+  duration: 2000,
+  distance: "90px",
+  delay: 1000,
+});
+
+// Sessão sobre
+revelar.reveal(".efeito-img-sobre", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+  origin: "left",
+});
+
+revelar.reveal(".efeito-texto-sobre", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+  origin: "right",
+});
+
+// Sessão projetos
+revelar.reveal(".efeito-img-projeto1", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+  origin: "left",
+});
+
+revelar.reveal(".efeito-img-projeto2", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+  origin: "top",
+});
+
+revelar.reveal(".efeito-img-projeto3", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+  origin: "bottom",
+});
+
+revelar.reveal(".efeito-img-projeto4", {
+  duration: 2000,
+  distance: "90px",
+  delay: 500,
+  origin: "right",
+});
+
+// Sessão formulario
+revelar.reveal(".input1", {
+  duration: 2000,
+  distance: "90px",
+  delay: 600,
+  origin: "left",
+});
+
+revelar.reveal(".input2", {
+  duration: 2000,
+  distance: "90px",
+  delay: 800,
+  origin: "right",
+});
+
+revelar.reveal(".input3", {
+  duration: 2000,
+  distance: "90px",
+  delay: 1000,
+  origin: "left",
+});
+
+revelar.reveal(".efeito-textarea", {
+  duration: 2000,
+  distance: "90px",
+  delay: 1200,
+  origin: "bottom",
+});
+
+revelar.reveal(".efeito-botao-enviar", {
+  duration: 2000,
+  distance: "90px",
+  delay: 1350,
+  origin: "bottom",
+});
